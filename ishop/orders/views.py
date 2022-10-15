8f99587e676f241452fde0c571540c5dd1d9d47a
VitaliiDomanskyi <115797529+VitaliiDomanskyi@users.noreply.github.com>
2022-10-15 10:24:04 +0300

    Update views.py

--- ishop/orders/views.py
+++ ishop/orders/views.py
@@ -16,8 +16,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
-            order_created.delay(order.id)
-            return render(request,
+                return render(request,
                           'orders/order/created.html',
                           {'order': order})
     else:
